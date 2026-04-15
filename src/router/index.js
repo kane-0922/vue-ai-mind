88e325221f8fe87c5d5234b9adb66bdb421e4ed5
coffee <2867162687@qq.com>
2026-04-15 21:40:20 +0800

    菜单基本样式实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import BackendLayout from '@/views/layout/BackendLayout.vue'
+
 // 路由配置
 const backendRoutes = [
   {
@@ -8,7 +9,35 @@ const backendRoutes = [
     children: [
       {
         path: 'dashboard',
-        component: () => import('@/views/dashboard/Dashboard.vue')
+        component: () => import('@/views/dashboard/Dashboard.vue'),
+        meta: {
+          title: '数据分析',
+          icon: 'PieChart'
+        }
+      },
+      {
+        path: 'knowledge',
+        component: () => import('@/views/knowledge/Knowledge.vue'),
+        meta: {
+          title: '知识文章',
+          icon: 'ChatLineSquare'
+        }
+      },
+      {
+        path: 'consultations',
+        component: () => import('@/views/consultations/Consultations.vue'),
+        meta: {
+          title: '咨询记录',
+          icon: 'Message'
+        }
+      },
+      {
+        path: 'emotional',
+        component: () => import('@/views/emotional/Emotional.vue'),
+        meta: {
+          title: '情绪日志',
+          icon: 'User'
+        }
       }
     ]
   }
--- src/views/layout/components/Sidebar.vue
+++ src/views/layout/components/Sidebar.vue
@@ -1,5 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const activeIndex = ref('2')
+
+const handleOpen = (key) => {
+  activeIndex.value = key
+}
+
+const handleClose = (key) => {
+  activeIndex.value = key
+}
+</script>
+
 <template>
-  <div>
-    菜单区域
-  </div>
+  <el-aside width="264px">
+    <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+      <el-menu-item v-for="item in router.options.routes[0].children" :key="item.path" :index="item.path">
+        <el-icon>
+          <component :is="item.meta.icon" />
+        </el-icon>
+        <span>{{ item.meta.title }}</span>
+      </el-menu-item>
+    </el-menu>
+  </el-aside>
 </template>
